8679ac37a3fe4169e22a66841c9770061c373de6
Dr. S. K. Choudhary <saket_au@rediffmail.com>
2024-11-20 09:33:58 +0530

    Update matrix.c

--- matrix.c
+++ matrix.c
@@ -1,15 +1,12 @@
 /* Program to create the two dimensional array (matrix )
 int A = [[3, 5, 11], [6, 4, 7]]
 */
-
 #include<stdio.h>
 void main()
 {
 	int A[2][3];    // Here matrix A has 2 rows and 3 columns
-	int row, column;
-	
+	int row, column;	
 	printf("------------------Please enter values of matrix ---------------\n");
-	
 	for(row = 0; row < 2 ; row++)
 	{
 		for(column = 0; column < 3; column++)
@@ -18,14 +15,12 @@ void main()
 			scanf("%d", &A[row][column]);
 		}
 	}
-	
 	printf("\n_______________Entered Matrix is ___________________\n");
-	
 	for(row = 0; row < 2 ; row++)
 	{
 		for(column = 0; column < 3; column++)
 		{
-			printf("\t %d", A[row][column]);
+			printf("\t %d", A[row][column])
 		}
 		printf("\n");
 	} 
